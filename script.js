f50b3f8c4fbde42eb0d2e37434da8d5952d624e6
MOHD NEHAL KHAN <nehalk482@gmail.com>
2023-11-18 15:52:08 +0530

    Added functioning of hamburger in js file & its styling in css

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const botBtn = document.querySelector(".bot-btn");
 botBtn.addEventListener("click", function () {
   window.location.href = "Homepage.html";
 });
+
+// hamburger icon working
+
+function onClickHamburger(){
+document.getElementById("hamburger").classList.toggle("cross-icon");
+document.getElementById("nav").classList.toggle("change-ham");
+
+const divToHide1 = document.querySelector(".hp-mid");
+divToHide1.style.display = "none";
+}
