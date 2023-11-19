71fd57cb14f75460410a4db8f55e26f47b58a226
MOHD NEHAL KHAN <nehalk482@gmail.com>
2023-11-19 19:16:09 +0530

    fix the background to display none after opening hamburger in js

--- script.js
+++ script.js
@@ -10,7 +10,26 @@ botBtn.addEventListener("click", function () {
 function onClickHamburger(){
 document.getElementById("hamburger").classList.toggle("cross-icon");
 document.getElementById("nav").classList.toggle("change-ham");
+const hamBurger = document.getElementById("hamburger");
+const navBar = document.getElementById("navigation");
+const hpImg = document.querySelector(".hp-mid");
+const hpFooter = document.querySelector(".hp-btn");
+const dhFooter = document.querySelector(".dh-footer")                     
 
-const divToHide1 = document.querySelector(".hp-mid");
-divToHide1.style.display = "none";
+  
+  if (hamBurger.classList.contains("cross-icon")) {
+    // Hamburger is now a cross, hide elements and set transparent background
+    hpImg.style.display = "none";
+    hpFooter.style.display = "none";
+    navBar.style.background = "none";
+    dhFooter.style.display = "none";
+  }
+     else 
+     {
+    // Hamburger is back, show elements and set yellow background
+    hpImg.style.display = "flex"; 
+    hpFooter.style.display = "flex";
+    navBar.style.background = "#faea58";
+    dhFooter.style.display = "flex"; 
+    }
 }
