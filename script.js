b6462094bf80db46390d4906ca253fd7a6ab6c1e
MOHD NEHAL KHAN <nehalk482@gmail.com>
2024-01-18 23:50:26 +0530

    Removed unwanted js code bcz delete & history page is no longer included

--- script.js
+++ script.js
@@ -50,28 +50,6 @@ function onClickHamburger() {
       taskIcon.style.display = "none";
       taskContainerDiv.style.display = "none";
     }
-
-    //condition for deleted task page
-    if (dhFooter && dhFooter.style.display == "block") {
-      if (hpImg) hpImg.style.display == "none";
-      if (tsAdd) tsAdd.style.display == "none";
-      dhFooter.style.display = "none";
-      hpBody.style.background = "#fff";
-    }
-    //condition for all deleted task page
-    if (Nores && Nores.style.display == "flex") {
-      if (Nores) Nores.style.display = "none";
-      if (hpBody) hpBody.style.background = "none";
-    }
-
-    //condition for history page
-    if (historyFooter && historyFooter.style.display == "flex") {
-      if (hpImg) hpImg.style.display == "none";
-      if (hpFooter) hpFooter.style.display == "none";
-      if (historyFooter) historyFooter.style.display = "none";
-      hpBody.style.background = "#fff";
-    }
-
   }
   else {
     // Hamburger is back, show elements and set yellow background
@@ -98,25 +76,6 @@ function onClickHamburger() {
       taskContainerDiv.style.display = "flex";
       taskIcon.style.zIndex = 0;
     }
-
-    if (dhFooter && dhFooter.style.display == "none") {
-      if (tsAdd) tsAdd.style.display == "none";
-      if (hpImg) hpImg.style.display = "none";
-      dhFooter.style.display = "block";
-      hpBody.style.background = "#6cb9e5";
-    }
-
-    if (Nores && Nores.style.display == "none") {
-      if (Nores) Nores.style.display = "flex";
-      if (hpBody) hpBody.style.background = "#fff";
-    }
-
-    if (historyFooter && historyFooter.style.display == "none") {
-      if (hpImg) hpImg.style.display = "none";
-      if (hpFooter) hpFooter.style.display = "none";
-      if (historyFooter) historyFooter.style.display = "flex";
-      if (hpBody) hpBody.style.background = "#6cb9e5";
-    }
   }
 }
 
@@ -135,17 +94,6 @@ function saveTask() {
   if (hpBody) hpBody.style.background = "#6cb9e5";
 }
 
-// Feature for Delete-Task icon
-function deleteTask() {
-  const dhFooter = document.querySelector(".dh-footer");
-  const hpImg = document.querySelector(".hp-mid");
-  const hpBody = document.querySelector(".hp");
-
-  if (dhFooter) dhFooter.style.display = "block";
-  if (hpImg) hpImg.style.display = "none";
-  if (hpBody) hpBody.style.background = "#6cb9e5";
-}
-
 // Feature for Add-Task icon
 function addTask() {
   const tsAdd = document.querySelector(".ts-add");
@@ -155,81 +103,6 @@ function addTask() {
   if (tsAdd) tsAdd.style.display = "flex";
   if (hpImg) hpImg.style.display = "none";
   if (hpFooter) hpFooter.style.display = "none";
-}
-
-// feature for hamburger if user click on save task
-
-function saveTasksl() {
-  const hpBody = document.querySelector(".hp");
-  const hpImg = document.querySelector(".hp-mid");
-  const hpFooter = document.querySelector(".hp-btn");
-  const hamBurger = document.getElementById("hamburger");
-  const navBar = document.getElementById("navigation");
-  const historyFooter = document.getElementById("history-footer");
-  const historyHeading = document.getElementById("history-head");
-  const navDelete = document.querySelector(".navs");
-
-  // Toggle the cross icon to hamburger
-  hamBurger.classList.toggle("cross-icon");
-
-  if (hamBurger.classList.contains("cross-icon")) {
-    if (hpImg) hpImg.style.display = "none";
-    if (hpFooter) hpFooter.style.display = "none";
-    if (hpBody) hpBody.style.background = "#6cb9e5";
-    if (historyFooter) historyFooter.style.display = "none";
-    if (historyHeading) historyHeading.style.display = "none";
-    if (navDelete && navDelete.classList.contains("change-ham")) {
-      navDelete.style.display = "flex";
-    }
-
-  } else {
-    if (hpImg) hpImg.style.display = "none";
-    if (hpFooter) hpFooter.style.display = "none";
-    if (hpBody) hpBody.style.background = "#6cb9e5";
-    if (historyFooter) historyFooter.style.display = "flex";
-    if (historyHeading) historyHeading.style.display = "flex";
-    if (navBar) navBar.style.background = "#faea58";;
-    if (navDelete && navDelete.classList.contains("change-ham")) {
-      navDelete.classList.remove("change-ham");
-      navDelete.style.display = "transparent";
-    }
-  }
-}
-
-// feature for hamburger if user click on delete task
-
-function deleteTasksl() {
-  const dhFooter = document.querySelector(".dh-footer");
-  const hpFooter = document.querySelector(".hp-btn");
-  const hpImg = document.querySelector(".hp-mid");
-  const hpBody = document.querySelector(".hp");
-  const hamBurger = document.getElementById("hamburger");
-  const navDelete = document.querySelector(".navs");
-  const navBar = document.getElementById("navigation");
-
-  // Toggle the cross icon to hamburger
-  hamBurger.classList.toggle("cross-icon");
-
-  if (hamBurger.classList.contains("cross-icon")) {
-    if (dhFooter) dhFooter.style.display = "none";
-    if (hpFooter) hpFooter.style.display = "none";
-    if (hpImg) hpImg.style.display = "none";
-    if (hpBody) hpBody.style.background = "#6cb9e5";
-    if (navDelete && navDelete.classList.contains("change-ham")) {
-      navDelete.style.display = "flex";
-    }
-  }
-  else {
-    if (dhFooter) dhFooter.style.display = "block";
-    if (hpFooter) hpFooter.style.display = "none";
-    if (navBar) navBar.style.background = "#faea58";
-    if (hpImg) hpImg.style.display = "none";
-    if (hpBody) hpBody.style.background = "#6cb9e5";
-    if (navDelete && navDelete.classList.contains("change-ham")) {
-      navDelete.classList.remove("change-ham");
-      navDelete.style.display = "transparent";
-    }
-  }
 }
 
 function createTask() {
@@ -313,14 +186,4 @@ function tsIcon() {
     tsaddHide.classList.remove("slide-down");
     taskIcon.style.display = "none";
   }
-}
-
-// Delete checked marks tasks
-
-function allDelete() {
-  const Nores = document.getElementById("noRes");
-  const hpBody = document.querySelector(".hp");
-
-  Nores.style.display = "flex";
-  hpBody.style.background = "#fff";
 }
