b8f1ea6a1db17c61efd0bb0c0fe445d69443bdbc
MOHD NEHAL KHAN <nehalk482@gmail.com>
2024-04-19 20:15:37 +0530

    Updated the js file

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// button that link index.html file to homepage.html file
-const botBtn = document.querySelector(".bot-btn");
-
-botBtn.addEventListener("click", function () {
-  window.location.href = "Homepage.html";
-});
-
 // hamburger icon working
 
 function onClickHamburger() {
